cd43e407f0a30bf692910fccb5cf069ed3e2b0cb
Vhutali <vhutali@pop-os.localdomain>
2020-08-10 00:53:26 +0200

    added db.sqlite3 file

--- users/models.py
+++ users/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.forms import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=256)
+    course_code = models.CharField(max_length=10, primary_key=True);
+
+    def __str__(self):
+        return '%s %s' %(self.course_code, self.name)
+
+
+    def get_course_name(self):
+        '''
+            Returns the name of the course
+        '''
+        return self.name
+
+    
+    def get_course_code(self):
+        '''
+            Returns the course code
+        '''
+        return self.course_code
+
+
+    
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    student_number = models.IntegerField(max_length=8, primary_key=True);
+    courses = models.ManyToManyField(Course)
+    password = models.CharField(max_length=256, default="password") # TODO this is just for testing purposes
+
+'''
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
+
+    def __str__(self):
+        return '%s, %s' %(self.name, self.student_number)
+    
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.student.save() # if you run into problem's try changing profile to student
+    
+'''
