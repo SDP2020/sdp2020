72ff77d8e981612eb9e98805693d20a58d9c122c
Vhutali <53083242+1715260@users.noreply.github.com>
2020-08-11 13:36:59 +0200

    Updated Course Test in users/test.py

--- users/tests.py
+++ users/tests.py
@@ -1,12 +1,10 @@
 from django.test import TestCase
-from users.models import Course
+from .models import Student, Course
 
 class CourseTestCase(TestCase):
-    def createCourse(self):
-        Course.objects.create(name="test_course", course_code="TEST0000")
-        Course.objects.create(name="test_course2", course_code="TEST0001")
-        print(Course.objects)
-        
+    def setUp(self):
+        Course.objects.create(name="TestCourse", course_code="TEST0000")
+    
     def testCourseExists(self):
-        course1 = Course.objects.get(course_code="TEST0001")
-        self.assertEqual(course1.get_course_name(), "test_course")
+        print("hello", Course.objects.all())
+        self.assertEqual(Course.objects.get(name="TestCourse", course_code="TEST0000").get_course_name(), "TestCourse")
