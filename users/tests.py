2e5b88b8fc4326fc4ed36383b742fd2cdc6ee435
Vhutali <vhutali@pop-os.localdomain>
2020-08-11 13:24:01 +0200

    added course test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from users.models import Course
 
-# Create your tests here.
+class CourseTestCase(TestCase):
+    def createCourse(self):
+        Course.objects.create(name="test_course", course_code="TEST0000")
+        Course.objects.create(name="test_course2", course_code="TEST0001")
+        print(Course.objects)
+        
+    def testCourseExists(self):
+        course1 = Course.objects.get(course_code="TEST0001")
+        self.assertEqual(course1.get_course_name(), "test_course")
