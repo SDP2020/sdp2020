7e482320d64ec9ea8a6ce4bd725990cc6893b53e
Pfunzo <1420895@students.wits.ac.za>
2020-08-25 10:43:17 +0200

    Test
    Test101

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Student, Course
 
-# Create your tests here.
+class CourseTestCase(TestCase):
+    def setUp(self):
+        Course.objects.create(name="TestCourse", course_code="TEST0000")
+    
+    def testCourseExists(self):
+        print("hello", Course.objects.all())
+        self.assertEqual(Course.objects.get(name="TestCourse", course_code="TEST0000").get_course_name(), "TestCourse")
