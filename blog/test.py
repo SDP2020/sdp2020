260c1aeee932a8fb462c5b28eb1d6054ade64802
Pfunzo <1420895@students.wits.ac.za>
2020-09-29 12:19:51 +0200

    Testing the block
    Testing everything

--- blog/test.py
+++ blog/test.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from .models import Post
+import unittest
+from django.test import Client
+from .urls import urlpatterns
+
 class PostTestCase(TestCase):
     def setUp(self):
         Post.objects.create(course_code="TESTCOR",course_name="Computer Sciences")
@@ -11,3 +15,14 @@ class PostTestCase(TestCase):
     def testPost2Exists(self):
         course_name= Post.objects.get(course_name="Computer Sciences")
         self.assertEqual(course_name.course_name,"Computer Sciences")
+
+class SimpleTest(unittest.TestCase):
+    def test_details(self):
+        client = Client()
+        response = client.get('/web_scraping/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_index(self):
+        client = Client()
+        response = client.get('/customer/index/')
+        self.assertEqual(response.status_code, 404)
